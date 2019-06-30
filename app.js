035ee432063cd3e5b980c90e28dcb73025be550e
Yanti Suryati <yantisc@gmail.com>
2019-06-30 10:38:21 +0100

    iterate the images objects API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const list = document.getElementById('items');
+const ul = document.getElementById('items');
 const url = "https://api.taboola.com/1.2/json/apitestaccount/recommendations.get?app.type=web&app.apikey=7be65fc78e52c11727793f68b06d782cff9ede3c&source.id=%2Fdigiday-publishing-summit%2F&source.url=https%3A%2F%2Fblog.taboola.com%2Fdigiday-publishing-summit%2F&source.type=text&placement.organic-type=mix&placement.visible=true&placement.available=true&placement.rec-count=6&placement.name=Below%20Article%20Thumbnails&placement.thumbnail.width=640&placement.thumbnail.height=480&user.session=init"
 
 function createNode(element) {
@@ -12,27 +12,40 @@ function append(parent, el) {
 
 
 fetch(url)
-  .then(function(resp){
-    return resp.json(); //transform the data into json
-  })
+  .then((resp) => resp.json()) //transform the data into json
   .then(function(data) {  // Create and append the list
     // console.log(data);
     let items = data.list
-      items.forEach (item => {
-        Object.entries(item).forEach( entry => {
-          let key  = entry[0];
-          let value = entry[1];
-          console.log(key.name);
-      const article_name = key.name;
-      list.innerHTML = `${article_name}`;
-        });
+      return items.map (item => {
+        const article_name = item.name;
+        const article_branding = item.branding;
+        const images = item.thumbnail
+          images.map(image => {
+            let article_image = image;
+            let imageUrl = (Object.values(article_image));
+
+
+        // let li   = createNode('li'),
+        //     img  = createNode ('img'),
+        let span = createNode ('span');
+        ul.innerHTML = `test`;
+        // append(li, img);
+        // append(li, span);
+        // append(ul,li);
+        console.log(article_name);
+        console.log(article_branding);
+        console.log (imageUrl[0]);
+
+      // let article_name = item.name;
+      // let article_branding = item.branding;
+      // let img = createNode ('img');
       })
     })
-      // const article_branding = item.branding;
-      // const img = createNode ('img');
+  })
 
-      //   console.log(item.thumbnail);
+  .catch(function(error){
 
+  });
 
       // console.log(items);
       // let li = createNode ('li');
